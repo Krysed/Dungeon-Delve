f2da89c7bbe0351f95a185fcc51d5982b3559607
Krystian Fryca <kp15731@gmail.com>
2023-03-07 10:56:45 +0100

    Character class & colision pre

---
+++ Dungeon Delve/game/src/Character.cpp
@@ -0,0 +1,61 @@
+#include "src/Character.h"
+
+
+Character::Character(int windowWidth,int windowHeight)
+{
+    width = texture.width;// / maxFrames;
+    height = texture.height;
+
+    screenPosition = {
+        static_cast<float>(windowWidth) / 2.f - scale * (0.5f * width / 6.f),
+        static_cast<float>(windowHeight) / 2.f - scale * (0.5f * width / 6.f)
+    };
+}
+Vector2 Character::getWorldPos() 
+{
+	return worldPosition;
+}
+
+
+
+void Character::tick(float deltaTime)
+{
+    worldPositionLastFrame = worldPosition;
+
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+    if (IsKeyDown(KEY_D)) direction.x += 1.0;
+    if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+    if (IsKeyDown(KEY_S)) direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        worldPosition = Vector2Add(worldPosition, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+
+    //update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames) frame = 0;
+    }
+
+    //drawing character
+    Rectangle source{ frame * width / 6.f,0.f,rightLeft * width / 6.f,height };
+    Rectangle destination{ screenPosition.x,screenPosition.y,4.0f * width / 6.f,4.0f * height };
+    DrawTexturePro(texture, source, destination, Vector2{}, 0.f, WHITE);
+
+}
+
+void Character::undoMovement()
+{
+    worldPosition = worldPositionLastFrame;
+}
--- Dungeon Delve/game/src/screen_gameplay.cpp
+++ Dungeon Delve/game/src/screen_gameplay.cpp
@@ -26,6 +26,7 @@
 #include "raylib.h"
 #include "screens.h"
 #include "raymath.h"
+#include "src/Character.h"
 
 //----------------------------------------------------------------------------------
 // Module Variables Definition (local)
@@ -58,72 +59,34 @@ void UpdateGameplayScreen(void)
     const int windowWidth{ 800 };
     const int windowHeight{ 600 };
 
-    Texture2D testMap = LoadTexture("resources/maps/test_map1.png");
-    Vector2 mapPosition{ 0.0,0.0 };
-    float movementSpeed = 4.f;
+    const float mapScale{4.0f};
 
-    Texture2D hero_idle = LoadTexture("resources/characters/knight_idle_spritesheet.png");
-    Texture2D hero_run = LoadTexture("resources/characters/knight_run_spritesheet.png");
+    Texture2D map = LoadTexture("resources/maps/test_map1.png");
+    Vector2 mapPosition{ 0.0f,0.0f };
+    Character player{windowWidth,windowHeight};
 
-    Texture2D hero = LoadTexture("resources/characters/knight_idle_spritesheet.png");
-    Vector2 heroPosition{
-        (float)windowWidth / 2.f - 4.0f * (0.5 * (float)hero.width / 6.f),
-        (float)windowHeight / 2.f - 4.0f * (0.5 * (float)hero.width / 6.f)
-    };
-    //direction
-    float rightLeft{ 1.f };
-    //animation variables
-    float runningTime{};
-    int frame{};
-    const int maxFrames{ 6 };
-    const float updateTime{ 1.f / 12.f };
-
-
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(WHITE);
-
-
-        Vector2 direction{};
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0;
-        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if (IsKeyDown(KEY_S)) direction.y += 1.0;
-        if (Vector2Length(direction) != 0.0) 
-        {
-            mapPosition = Vector2Subtract(mapPosition,Vector2Scale(Vector2Normalize(direction),movementSpeed));
-            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-            hero = hero_run;
-        }
-        else
-        {
-            hero = hero_idle;
-        }
-
+    while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
         {
             finishScreen = 1;
             PlaySound(fxCoin);
         }
+
+        mapPosition = Vector2Scale(player.getWorldPos(), -1.f);
         //Drawing map
-        DrawTextureEx(testMap, mapPosition, 0, 4, WHITE);
+        DrawTextureEx(map, mapPosition, 0.0f, mapScale, WHITE);
+        player.tick(GetFrameTime());
 
-        //update animation frame
-        runningTime += GetFrameTime();
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 0;
+        //checking map bounds
+        if (
+            player.getWorldPos().x < 0.f ||
+            player.getWorldPos().y < 0.f ||
+            player.getWorldPos().x + windowWidth > map.width * mapScale ||
+            player.getWorldPos().y + windowHeight > map.height * mapScale
+            ) {
+            player.undoMovement();
         }
-
-        //drawing character
-        Rectangle source{ frame*(float)hero.width / 6.f,0.f,rightLeft*(float)hero.width / 6.f,(float)hero.height };
-        Rectangle destination{ heroPosition.x,heroPosition.y,4.0f * (float)hero.width / 6.f,4.f * (float)hero.height };
-        DrawTexturePro(hero, source, destination, Vector2{}, 0.f, WHITE);
-
+        
         EndDrawing();
     }
 }
@@ -133,10 +96,11 @@ void DrawGameplayScreen(void)
 {
     // TODO: Draw GAMEPLAY screen here!
     
-    /*DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
-    Vector2 pos = { 20, 10 };
+    /*
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
     DrawTextEx(font, "GAMEPLAY SCREEN", pos, font.baseSize*3.0f, 4, MAROON);
-    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);*/
+    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+    */
 
 }
 
