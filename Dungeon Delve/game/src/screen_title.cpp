fb0023fbc6968f8fe9d566ea38d7f66e3ad09a61
HS17Official <hs17@onet.pl>
2023-04-16 12:05:40 +0200

    Added title screen modifications

--- Dungeon Delve/game/src/screen_title.cpp
+++ Dungeon Delve/game/src/screen_title.cpp
@@ -1,11 +1,8 @@
-/**********************************************************************************************
+/***************************************************************************************************
+*   Raylib - Advance Game template.
 *
-*   raylib - Advance Game template
-*
-*   Title Screen Functions Definitions (Init, Update, Draw, Unload)
-*
-*   Copyright (c) 2014-2022 Ramon Santamaria (@raysan5)
-*
+*   Title Screen Functions Definitions (Init, Update, Draw, Unload).
+*   Copyright (c) 2014-2022 Ramon Santamaria (@raysan5).
 *   This software is provided "as-is", without any express or implied warranty. In no event
 *   will the authors be held liable for any damages arising from the use of this software.
 *
@@ -15,26 +12,29 @@
 *     1. The origin of this software must not be misrepresented; you must not claim that you
 *     wrote the original software. If you use this software in a product, an acknowledgment
 *     in the product documentation would be appreciated but is not required.
-*
 *     2. Altered source versions must be plainly marked as such, and must not be misrepresented
 *     as being the original software.
-*
 *     3. This notice may not be removed or altered from any source distribution.
-*
-**********************************************************************************************/
+***************************************************************************************************/
 
 #include "raylib.h"
 #include "screens.h"
+#include <external/stb_image.h>
 
-//----------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 // Module Variables Definition (local)
-//----------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
+
 static int framesCounter = 0;
 static int finishScreen = 0;
+static Image image;
+static Texture2D background;
+static float alpha = 0.0f;
+static bool isIncreasing = true;
 
-//----------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 // Title Screen Functions Definition
-//----------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 
 // Title Screen Initialization logic
 void InitTitleScreen(void)
@@ -42,6 +42,9 @@ void InitTitleScreen(void)
     // TODO: Initialize TITLE screen variables here!
     framesCounter = 0;
     finishScreen = 0;
+    image = LoadImage("resources/screens/DungeonDelve.png");
+    ImageResize(&image, 800, 600);
+    background = LoadTextureFromImage(image);
 }
 
 // Title Screen Update logic
@@ -52,20 +55,43 @@ void UpdateTitleScreen(void)
     // Press enter or tap to change to GAMEPLAY screen
     if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
     {
-        //finishScreen = 1;   // OPTIONS
-        finishScreen = 2;   // GAMEPLAY
+        //finishScreen = 1; // OPTIONS
+        finishScreen = 2; // GAMEPLAY
         PlaySound(fxCoin);
     }
+
 }
 
 // Title Screen Draw logic
 void DrawTitleScreen(void)
 {
     // TODO: Draw TITLE screen here!
-    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), GREEN);
+    DrawTexture(background, 0, 0, WHITE);
     Vector2 pos = { 20, 10 };
-    DrawTextEx(font, "Dungeon Delve", pos, font.baseSize*3.0f, 4, DARKGREEN);
-    DrawText("PRESS ENTER or TAP to SPACE BAR to GAMEPLAY SCREEN", 90, 220, 20, DARKGREEN);
+    DrawText("PRESS ENTER TO START", 268, 27, 20, WHITE);
+
+    //Animation of text
+    if (isIncreasing)
+    {
+        alpha += 0.01f;
+        if (alpha > 1.0f)
+        {
+            alpha = 1.0f;
+            isIncreasing = false;
+        }
+    }
+    else
+    {
+        alpha -= 0.01f;
+        if (alpha < 0.0f)
+        {
+            alpha = 0.0f;
+            isIncreasing = true;
+        }
+    }
+
+    Color rectColor = { 0, 0, 0, (unsigned char)(alpha * 255) };
+    DrawRectangle(0, 0, GetScreenWidth(), 43, rectColor);
 }
 
 // Title Screen Unload logic
